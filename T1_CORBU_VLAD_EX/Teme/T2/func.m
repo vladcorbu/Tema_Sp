ef34f019aafa1a1dea2dca233aa26591851a5713
Corbu Vlad <corbuvlad@gmail.com>
2019-11-01 01:07:39 +0200

    Update func.m

--- T1_CORBU_VLAD_EX/Teme/T2/func.m
+++ T1_CORBU_VLAD_EX/Teme/T2/func.m
@@ -1,4 +1,4 @@
 function [x, t] = func(rez,T) %T perioada rez, rezolutia
   t = 0:rez:T*5;   %atribuim rezolutia temporala vreau sa vad 5 perioade
-  x = 1.5*sawtooth(2 * pi * 1/T * t , 0.5) - 0.5; %0.2 este frecventa cu o locatie maxima a semnalului de 0.5 ce determina punctul intre 0 si 2pi unde semnalul isi atinge maximul in cazul semnalului triunghiular la 0.5
+  x = 1.5*sawtooth(2 * pi * 1/T * t , 0.6) - 0.5; %0.2 este frecventa, 0.6 determina punctul intre 0 si T unde semnalul isi atinge maximul(adica la 6/10 din perioada aici panta este 1)
 end
