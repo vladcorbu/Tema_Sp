3ef8ac644ec8750aaac3b5d8bfcd1e5bc3a74bab
Corbu Vlad <corbuvlad@gmail.com>
2019-11-01 17:44:15 +0200

    Update T3.m

--- T1_CORBU_VLAD_EX/Teme/T3/T3.m
+++ T1_CORBU_VLAD_EX/Teme/T3/T3.m
@@ -3,22 +3,22 @@ cnt = 1;
 for k = 1:numel(v)
   figure(cnt);
   [x,t] = func(v(k),0.25,8,[-1,1]);
-  plot(t,x,'.-'),xlabel('Timp [s]'),ylabel('Amplitudine'),title('multinivel a'),grid;
+  plot(t,x,'.-'),xlabel('Timp [s]'),ylabel('Amplitudine'),title(['multinivel a ', num2str(v(k))]),grid;
   cnt =cnt+1;
 
   figure(cnt);
   [x,t] = func(v(k),0.25,8,[-3,-1,1,3]);
-  plot(t,x,'.-'),xlabel('Timp [s]'),ylabel('Amplitudine'),title('multinivel b'),grid;
+  plot(t,x,'.-'),xlabel('Timp [s]'),ylabel('Amplitudine'),title(['multinivel b ', num2str(v(k))]),grid;
   cnt = cnt+1;
 
   figure(cnt);
   [x,t] = func(v(k),0.25,8,[-5,-3,-1,1,3,5]);
-  plot(t,x,'.-'),xlabel('Timp [s]'),ylabel('Amplitudine'),title('multinivel c'),grid;
+  plot(t,x,'.-'),xlabel('Timp [s]'),ylabel('Amplitudine'),title(['multinivel c ', num2str(v(k))]),grid;
   cnt = cnt+1;
   
   figure(cnt);
   [x,t] = func(v(k),0.25,8,[-7,-5,-3,-1,1,3,5,7]);
-  plot(t,x,'.-'),xlabel('Timp [s]'),ylabel('Amplitudine'),title('multinivel d'),grid;
+  plot(t,x,'.-'),xlabel('Timp [s]'),ylabel('Amplitudine'),title(['multinivel d ', num2str(v(k))]),grid;
   cnt =cnt+1;
 end
 %semnalul in trepte cel mai bine se vede la o rezonanta de 0.02
